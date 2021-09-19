ad0a7080485e7963edb875e3d8ff40a2ff7f87fc
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-19 23:46:01 +0300

    adit handl

--- src/index.js
+++ src/index.js
@@ -1,43 +1,40 @@
+import './css/styles.css';
 import refs from './js/refs';
-import menuTemplates from './templates/templates.handlebars'
-import menu from '../menu.json/';
+import menuTemplates from './templates/templates.hbs';
+import menu from '../menu.json';
 const { switchToggle, body, listMenu, switchControl } = refs;
-
 const addListItem = menu.map((object) => {
     const { id, name, description, image, price } = object;
     const markup = menuTemplates(object);
-    listMenu.insertAdjacentHTML('beforeend', markup)
+    listMenu.insertAdjacentHTML('beforeend', markup);
 });
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
-const refBody = document.querySelector('body')
-const refCheckBox = refBody.querySelector('.theme-switch__control')
-const refInput = refCheckBox.querySelector('.theme-switch__toggle')
-const refMenu = refBody.querySelector('.js-menu');
-console.log(refCheckBox)
-const localStorageValue = localStorage.getItem('Theme') || Theme.LIGHT
-refInput.setAttribute("checked", "false")
-refInput.checked = localStorageValue === Theme.DARK
+const localStorageThem = localStorage.getItem('Theme') || Theme.LIGHT;
+switchToggle.setAttribute("checked", "false");
+switchToggle.checked = localStorageThem === Theme.DARK;
 
-addClass(localStorageValue)
+addClass(localStorageThem);
 
-function addClass(color) {
-    refBody.classList.add(color)
+function addClass(theme) {
+    body.classList.add(theme);
 }
 switchControl.addEventListener('change', onClickHolderThem);
-function onClickHolderThem(evt) {
-    removeValue(Theme.LIGHT, Theme.DARK)
-    if (evt.target.checked) {
-        localStorage.setItem("Theme", 'dark-theme');
-    } else {
-        localStorage.removeItem('Theme')
-    }
-}
 
+function onClickHolderThem(evt) {
+    if (evt.target.checked) {
+        localStorage.setItem('Theme', 'dark-theme');
+    } else {
+        localStorage.removeItem('Theme');
+    }
+    removeValue(Theme.LIGHT, Theme.DARK);
+
+}
 function removeValue(light, dark) {
-    body.classList.toggle(light)
-    body.classList.toggle(dark)
+    body.classList.toggle(light);
+    body.classList.toggle(dark);
+
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 export default {
-    switchToggle: document.querySelector('.theme-switch-toggle'),
-    switchControl: document.querySelector('.theme-switch__control'),
     body: document.querySelector('body'),
+    switchControl: document.querySelector('.theme-switch__control'),
+    switchToggle: document.getElementById('theme-switch-toggle'),
     listMenu: document.querySelector('.js-menu')
-
 }
