b362721630573073fdcdef5c57ca4f984474127b
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-20 16:43:06 +0300

    Updates

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export default {
-    body: document.querySelector('body'),
-    switchControl: document.querySelector('.theme-switch__control'),
-    switchToggle: document.getElementById('theme-switch-toggle'),
-    listMenu: document.querySelector('.js-menu')
-}
